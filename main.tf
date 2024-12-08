be9bd6e462fb22ec374025016cc4254f616344f7
ronel <ronel1354@gmail.com>
2024-12-08 19:15:31 +0200

    delete line 73 from main.tf

--- main.tf
+++ main.tf
@@ -69,11 +69,6 @@ resource "aws_codedeploy_deployment_group" "production_deployment" {
   deployment_config_name = "CodeDeployDefault.OneAtATime"
 }
 
-# Step Functions State Machine for Multi-stage Deployment
-resource "aws_sfn_state_machine" "deployment" {
-  name     = "DeploymentStateMachine"
-  role_arn = aws_iam_role.step_functions_execution_role.arn
-
   definition = jsonencode({
     StartAt = "ValidateCode",
     States = {
