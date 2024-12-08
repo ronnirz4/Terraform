fb56c690a526162740694f6cdb02c9667fec12a0
ronel <ronel1354@gmail.com>
2024-12-08 19:07:56 +0200

    update main

--- main.tf
+++ main.tf
@@ -3,71 +3,6 @@ provider "aws" {
   region = "us-east-2"
 }
 
-# Create IAM Role for Lambda Execution
-resource "aws_iam_role" "lambda_exec_role" {
-  name = "lambda_execution_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action    = "sts:AssumeRole"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-        Effect    = "Allow"
-        Sid       = ""
-      },
-    ]
-  })
-}
-
-# Lambda Permissions Policy
-resource "aws_iam_policy" "lambda_permissions" {
-  name        = "LambdaPermissions"
-  description = "Permissions for Lambda to interact with other services"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action   = ["s3:GetObject", "codepipeline:PutJobSuccessResult"]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-      {
-        Action   = "iam:PassRole"
-        Effect   = "Allow"
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
-  role       = aws_iam_role.lambda_exec_role.name
-  policy_arn = aws_iam_policy.lambda_permissions.arn
-}
-
-# Create Lambda Functions for Validate Code and Run Tests
-resource "aws_lambda_function" "validate_code" {
-  filename         = "validate_code.zip"
-  function_name    = "validate_code"
-  role             = aws_iam_role.lambda_exec_role.arn
-  handler          = "index.handler"
-  runtime          = "nodejs14.x"
-  source_code_hash = filebase64sha256("validate_code.zip")
-}
-
-resource "aws_lambda_function" "run_tests" {
-  filename         = "run_tests.zip"
-  function_name    = "run_tests"
-  role             = aws_iam_role.lambda_exec_role.arn
-  handler          = "index.handler"
-  runtime          = "nodejs14.x"
-  source_code_hash = filebase64sha256("run_tests.zip")
-}
-
 # Create IAM Role for Step Functions Execution
 resource "aws_iam_role" "step_functions_execution_role" {
   name = "step_functions_execution_role"
@@ -109,6 +44,7 @@ resource "aws_iam_policy" "step_functions_permissions" {
   })
 }
 
+# Attach Policy to Step Functions IAM Role
 resource "aws_iam_role_policy_attachment" "step_functions_policy_attach" {
   role       = aws_iam_role.step_functions_execution_role.name
   policy_arn = aws_iam_policy.step_functions_permissions.arn
@@ -122,14 +58,14 @@ resource "aws_codedeploy_app" "app" {
 resource "aws_codedeploy_deployment_group" "staging_deployment" {
   app_name              = aws_codedeploy_app.app.name
   deployment_group_name = "staging-deployment-group"
-  service_role          = aws_iam_role.lambda_exec_role.arn
+  service_role          = aws_iam_role.step_functions_execution_role.arn
   deployment_config_name = "CodeDeployDefault.OneAtATime"
 }
 
 resource "aws_codedeploy_deployment_group" "production_deployment" {
   app_name              = aws_codedeploy_app.app.name
   deployment_group_name = "production-deployment-group"
-  service_role          = aws_iam_role.lambda_exec_role.arn
+  service_role          = aws_iam_role.step_functions_execution_role.arn
   deployment_config_name = "CodeDeployDefault.OneAtATime"
 }
 
@@ -137,6 +73,7 @@ resource "aws_codedeploy_deployment_group" "production_deployment" {
 resource "aws_sfn_state_machine" "deployment" {
   name     = "DeploymentStateMachine"
   role_arn = aws_iam_role.step_functions_execution_role.arn
+
   definition = jsonencode({
     StartAt = "ValidateCode",
     States = {
