d1c20a2f95c1813c46292d58af74343a3f465cdc
ronel <ronel1354@gmail.com>
2024-12-17 21:03:03 +0200

    update file

--- main.tf
+++ main.tf
@@ -360,7 +360,7 @@ resource "aws_codepipeline" "pipeline" {
       output_artifacts = ["SourceOutput"]
       configuration = {
         S3Bucket    = "ronn4-staging-bucket"
-        S3ObjectKey = "your-source-code.zip"
+        S3ObjectKey = "staging.zip"
       }
       version = "1"
     }
